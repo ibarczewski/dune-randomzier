fa9c80e52c96bcadb4687bead810d40355514e68
ibarczew@cisco.com <ian.barczewski@wwt.com>
2021-09-01 19:54:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NamePlate } from "./NamePlate";
+import * as _ from "lodash";
 
 function App() {
+  const names = ["Zoey", "Ian", "Joel", "Zach", "Matt", "Ed"];
+  const shuffledFactions = _.shuffle([
+    "Atreides",
+    "Harkonnen",
+    "Spacing Guild",
+    "Emperor",
+    "Fremen",
+    "Bene Gesserit",
+    "Tleilaxu",
+    "Ixian",
+  ]);
+  const assignments = [];
+
+  names.forEach((name) => {
+    const faction = shuffledFactions.pop();
+    assignments.push({ name, faction });
+  });
+
+  const topRow = assignments[0];
+  const secondRow = [assignments[1], assignments[2]];
+  const thirdRow = [assignments[3], assignments[4]];
+  const bottomRow = assignments[5];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Dune Randomizer</header>
+      <div className="player-wrapper">
+        {
+          <NamePlate
+            position="top-row"
+            name={topRow.name}
+            faction={topRow.faction}
+          ></NamePlate>
+        }
+      </div>
+      <div className="player-wrapper">
+        {secondRow.map(({ name, faction }) => (
+          <NamePlate
+            position="middle-row"
+            name={name}
+            faction={faction}
+          ></NamePlate>
+        ))}
+      </div>
+      <div className="player-wrapper">
+        {thirdRow.map(({ name, faction }) => (
+          <NamePlate
+            position="middle-row"
+            name={name}
+            faction={faction}
+          ></NamePlate>
+        ))}
+      </div>
+      <div className="player-wrapper">
+        {
+          <NamePlate
+            position="bottom-row"
+            name={bottomRow.name}
+            faction={bottomRow.faction}
+            stormStart={true}
+          ></NamePlate>
+        }
+      </div>
     </div>
   );
 }
---
+++ src/NamePlate.js
@@ -0,0 +1,11 @@
+import './NamePlate.css';
+
+const NamePlate = ({name, faction, position, stormStart}) => {
+    return <div className={`${position} name-plate`}>
+        <div class="player-name">{name}</div>
+        <div>{faction}</div>
+        {stormStart ? <div className="storm-start">*storm start</div> : ''}
+    </div>
+}
+
+export { NamePlate };
